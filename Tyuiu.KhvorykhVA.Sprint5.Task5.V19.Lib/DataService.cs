cdbb69128e689511a3a507c75973c2fabbb42c32
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-11-25 18:26:26 +0500

    Task5

--- Tyuiu.KhvorykhVA.Sprint5.Task5.V19.Lib/DataService.cs
+++ Tyuiu.KhvorykhVA.Sprint5.Task5.V19.Lib/DataService.cs
@@ -1,29 +1,34 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
-using System.IO;
 using System.Globalization;
+using System.IO;
+
 namespace Tyuiu.KhvorykhVA.Sprint5.Task5.V19.Lib
 {
     public class DataService : ISprint5Task5V19
     {
         public double LoadFromDataFile(string path)
         {
-            var culture = CultureInfo.InvariantCulture;
+            double res = 0;
+            var culture = new CultureInfo("ru-RU"); 
 
             using (StreamReader reader = new StreamReader(path))
             {
-                string line = reader.ReadLine(); 
+                string line = reader.ReadLine();
                 if (line != null)
                 {
                     string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    if (double.TryParse(parts[0], NumberStyles.Float, culture, out double firstNumber) &&
-                        double.TryParse(parts[1], NumberStyles.Float, culture, out double secondNumber))
+                    if (parts.Length >= 2)
                     {
-                        return firstNumber + secondNumber;
+                        if (double.TryParse(parts[0], NumberStyles.Float, culture, out double firstNumber) &&
+                            double.TryParse(parts[1], NumberStyles.Float, culture, out double secondNumber))
+                        {
+                            res = firstNumber + secondNumber; 
+                        }
                     }
                 }
             }
-            return 0;
+            return res;
         }
     }
 }
