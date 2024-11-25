fad28682d75269f046866150fd06e8cc4c08c77a
Viktor Khvorih <Tyuiu.KhvorihVA@outlook.com>
2024-11-25 18:10:26 +0500

    Task5

--- Tyuiu.KhvorykhVA.Sprint5.Task5.V19.Lib/DataService.cs
+++ Tyuiu.KhvorykhVA.Sprint5.Task5.V19.Lib/DataService.cs
@@ -7,31 +7,23 @@ namespace Tyuiu.KhvorykhVA.Sprint5.Task5.V19.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            double res = 0;
-            var culture = CultureInfo.InvariantCulture; 
+            var culture = CultureInfo.InvariantCulture;
 
             using (StreamReader reader = new StreamReader(path))
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                string line = reader.ReadLine(); 
+                if (line != null)
                 {
                     string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    foreach (var part in parts)
+                    if (double.TryParse(parts[0], NumberStyles.Float, culture, out double firstNumber) &&
+                        double.TryParse(parts[1], NumberStyles.Float, culture, out double secondNumber))
                     {
-                        if (double.TryParse(part, NumberStyles.Float, culture, out double number))
-                        {
-                            res += number;
-                        }
-                        else
-                        {
-
-                            Console.WriteLine($"Некорректная строка или число: {part}");
-                        }
+                        return firstNumber + secondNumber;
                     }
                 }
             }
-            return res;
+            return 0;
         }
     }
 }
